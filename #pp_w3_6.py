cae564074a4170ae5931e07aa2fc538a775b5bd4
preetcool <preet81196@gmail.com>
2020-05-20 21:38:21 +0530

    list value

---
+++ #pp_w3_6.py
@@ -0,0 +1,11 @@
+"""Write a Python program which accepts a sequence of comma-separated numbers from user and generate a list and a tuple with those numbers.
+
+Sample data: 3, 5, 7, 23"""
+
+val = input("enter number ")
+
+list = val.split(",")
+tuple = tuple(list)
+
+print(list)
+print(tuple)
