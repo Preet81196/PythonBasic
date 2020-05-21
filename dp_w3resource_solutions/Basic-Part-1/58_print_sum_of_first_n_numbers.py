92c0d9135a8eed132a32c4543f77f29ef3ed7c0f
DivYesh Patel <pateldivyesh009@gmail.com>
2020-05-21 09:21:43 +0530

    performance improve in summation code

--- dp_w3resource_solutions/Basic-Part-1/58_print_sum_of_first_n_numbers.py
+++ dp_w3resource_solutions/Basic-Part-1/58_print_sum_of_first_n_numbers.py
@@ -11,12 +11,21 @@ answer = sum([each for each in range(n+1)])
 print('Sum:', answer)
 # print(timeit.timeit('sum([each for each in range(20)])', number=1))  # 4.500000000018378e-06
 
+# Other way
+# answer = sum(range(n+1))
+# print('Sum:', answer)
+# print(timeit.timeit('sum(range(20))', number=1))  # 2.012999999967402e-06
+
+# Upper and Lower code both are same, complexity is also same, running just a single loop and adds to a variable
+# Although Sum function is built-in and pre-compiled so its a faster solution
+# Otherwise, creating a list and then returning its elements sum employs 2 loops, (creating and finding sum)
 
 # Another way
 # def main():
 #     answer = 0
 #     for each in range(20):
 #         answer += each
+#     return answer
 #
 #
 # print(timeit.timeit('main()', setup='from __main__ import main', number=1))  # 3.248000000011242e-06
