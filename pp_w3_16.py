8e93f51379983224cd17d0b4fa06ec79d50a1a4b
preetcool <preet81196@gmail.com>
2020-05-21 15:31:06 +0530

    diff_of _two_num

--- pp_w3_16.py
+++ pp_w3_16.py
@@ -4,7 +4,7 @@ if the number is greater than 17 return double the absolute difference.
 """
 num = int(input("enter number : "))
 
-if num > 17:
-        print("num is ",(num-17)*2)
+if num <= 17:
+        print("num is ",(17-num))
 else:
-        print("num is",17-num)
+        print("num is",(num -17)*2)
