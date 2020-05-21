37b3694829323336d3236ff04c1e555ee0453c5b
preetcool <preet81196@gmail.com>
2020-05-21 16:05:35 +0530

    sum_of 3_no....

---
+++ pp_w3_18.py
@@ -0,0 +1,8 @@
+num1 = int(input("enter number 1 :"))
+num2 = int(input("enter number 3 :"))
+num3 = int(input("enter number 3 :"))
+
+if num1 == num2 == num3:
+    print((num1+num2+num3)*3)
+else:
+    print(num1+num2+num3)
