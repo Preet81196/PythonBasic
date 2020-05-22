b21a5aeb76f59e7e45ba9cbaf45a757f279bc7d7
preetcool <preet81196@gmail.com>
2020-05-22 20:56:22 +0530

    sum_of_num_or_if_2num_are_same_then_0

---
+++ pp_w3_33.py
@@ -0,0 +1,9 @@
+a = int(input("enter number 1  :"))
+b = int(input("enter number 2  :"))
+c = int(input("enter number 3  :"))
+
+if a==b or b==c or c==a:
+    print("this is zero :",0)
+else:
+    print("sum of 3 num :",a+b+c)
+    
