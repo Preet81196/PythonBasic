d9face7d0db93e69944a163ba90df84f05ddb311
DivYesh Patel <pateldivyesh009@gmail.com>
2020-05-23 07:28:20 +0530

    bug fix

--- dp_w3resource_solutions/Basic-Part-1/24_given_char_vowel_or_not.py
+++ dp_w3resource_solutions/Basic-Part-1/24_given_char_vowel_or_not.py
@@ -3,9 +3,11 @@ Write a Python program to test whether a passed letter is a vowel or not.
 """
 vowel_list = ['a', 'e', 'i', 'o', 'u']
 
-def isVowel(ch):
-    if ch in vowel_list:
+
+def is_vowel(ch):
+    if ch.lower() in vowel_list:
         return True
     return False
 
-print(isVowel('u'))
+
+print(is_vowel('u'))
