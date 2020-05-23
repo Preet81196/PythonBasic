006ca53c3943646ec9cbdc32b540ef16a83cc742
preetcool <preet81196@gmail.com>
2020-05-23 11:18:28 +0530

    rename

---
+++ pp_w3resource_solutions/basic_part_1/pp_38_equation.py
@@ -0,0 +1,13 @@
+"""def equation(x,y):
+    result = x * x + 2 * x * y + y * y
+    print(result)
+equation(3,4)
+"""
+
+
+
+x = 3
+y = 4
+
+result = x * x + 2 * x * y + y * y
+print("( {} + {} ) ** 2 = {}" .format(x,y,result))
