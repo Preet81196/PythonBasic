b1e370c653fbb709ec9c7ff580b7e33befe82d7b
DivYesh Patel <pateldivyesh009@gmail.com>
2020-05-23 21:54:02 +0530

    saturday exercises

---
+++ dp_w3resource_solutions/functions/18_execute_python_code.py
@@ -0,0 +1,10 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 23/05/20
+* @decription: Write a Python program to execute a string containing Python code.
+"""
+
+
+my_code = "print('hello world')"
+exec(my_code)
