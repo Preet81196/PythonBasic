5f2fe034ab4011349ca9fa8a830706fff9f79fb8
preetcool <preet81196@gmail.com>
2020-05-23 14:22:43 +0530

    sys_info

---
+++ pp_w3resource_solutions/basic_part_1/pp_43_sys_info.py
@@ -0,0 +1,11 @@
+import os
+import platform
+
+print(os.name)
+print(platform.system())
+print(platform.uname())
+print(platform.version())
+print(platform.win32_is_iot())
+print(platform.win32_ver())
+print(platform.processor())
+print(platform.uname())
