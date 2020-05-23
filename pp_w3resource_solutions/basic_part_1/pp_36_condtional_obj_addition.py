27729c7755d44cdc4374be8f5d3c4d80e07154f6
preetcool <preet81196@gmail.com>
2020-05-23 10:13:35 +0530

    rename

---
+++ pp_w3resource_solutions/basic_part_1/pp_36_condtional_obj_addition.py
@@ -0,0 +1,10 @@
+def  sum(a,b):
+        
+        if isinstance(a,int) and isinstance(b,int):
+                print(a+b)
+        else:
+                print("this is not int number,please enter int number")
+
+
+sum(4,'7')
+    
---
+++ pp_w3resource_solutions/basic_part_1/pp_37_print_personal_details.py
@@ -0,0 +1,6 @@
+def my_details():
+    name,age = "preet" ,24
+   
+    address ="sisva"
+    print("name:{} \nage:{}\naddress:{}".format(name,age,address))
+my_details()
