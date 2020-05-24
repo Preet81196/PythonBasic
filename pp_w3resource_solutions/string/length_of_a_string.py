5ac5ff984c7a3b1c91c7e5a8ddff8ee4be971c81
preetcool <preet81196@gmail.com>
2020-05-24 10:23:16 +0530

    string_length

--- pp_w3resource_solutions/string/length_of_a_string.py
+++ pp_w3resource_solutions/string/length_of_a_string.py
@@ -1,7 +1,10 @@
-
+"""
 def string_length(str1):
     count = 0
-    for char in str1:
+    for i in str1:
         count += 1
     return count
-print(string_length('preet'))
+print(string_length('preet'))"""
+
+q = ("preet patel")
+print(len(q))
