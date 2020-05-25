c43457cc1c4d45545cd1e788a02af861730e850e
preetcool <preet81196@gmail.com>
2020-05-25 11:05:40 +0530

    HTML_string

---
+++ pp_w3resource_solutions/string/15_HTML_string.py
@@ -0,0 +1,9 @@
+print("<{0}> python <{1}>".format("i","/i"))
+
+"""
+def add_tags(tag, word):
+    	return "<%s>%s</%s>" % (tag, word, tag)
+print(add_tags('i', 'Python'))
+print(add_tags('b', 'Python Tutorial'))
+
+"""
