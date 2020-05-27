d48cff85ad67d3a910c36ce15b18b4f0831ec59a
DivYesh Patel <pateldivyesh009@gmail.com>
2020-05-27 16:31:41 +0530

    list exercises

---
+++ dp_w3resource_solutions/list/25_randomly_pick_element_from_list.py
@@ -0,0 +1,12 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 27/05/20
+* @decription: Write a Python program to select an item randomly from a list.
+"""
+
+import random
+alist = ['divyesh', 'patel', 'prit', 'sagar', 'rajalaxmi', 'subramaniam']
+
+# TODO: Can do this manually, by generating a random integer
+print(random.choice(alist))
