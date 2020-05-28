573849a8d6b04c28abb344895231683c8889d4a4
preetcool <preet81196@gmail.com>
2020-05-28 15:37:23 +0530

    check this method

--- dp_w3resource_solutions/list/6_sort_list_conditionally.py
+++ dp_w3resource_solutions/list/6_sort_list_conditionally.py
@@ -17,3 +17,14 @@ alist = [(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)]
 
 print(sorted(alist, key=lambda each: each[1]))
 # print(sorted(alist, key=itemgetter(1)))  # Another way using a module
+
+"""#secound method 
+# tuples by the second Item using sort() 
+
+def Sort_Tuple(tup):
+	
+	tup.sort(key = lambda x: x[1]) 
+	return tup 
+ 
+print(Sort_Tuple([(2, 5), (1, 2), (4, 4), (2, 3), (2, 1)])) """
+
