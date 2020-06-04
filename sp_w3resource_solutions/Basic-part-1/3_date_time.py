3e1c301d7beec9c47ebb58c779a9d30d678b28ab
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-06-04 23:17:30 +0530

    3_date_time

---
+++ sp_w3resource_solutions/Basic-part-1/3_date_time.py
@@ -0,0 +1,8 @@
+"""Write a Python program to display the current date and time.
+Sample Output :
+Current date and time :
+2014-07-05 14:34:14 """
+
+import datetime
+current=datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+print("current time is",current)
