69ba833db2dbefaac4ba84750b5c47031ceaa312
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-05 00:27:17 +0530

    dictionary exercise

---
+++ dp_w3resource_solutions/dictionary/2_add_key_value_pair.py
@@ -0,0 +1,11 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 05/06/20
+* @decription: Write a Python script to add a key to a dictionary.
+"""
+
+adict = {1: 'Divyesh', 2: 'Tesla'}
+adict[3] = 'SpaceX'
+
+print(adict)
