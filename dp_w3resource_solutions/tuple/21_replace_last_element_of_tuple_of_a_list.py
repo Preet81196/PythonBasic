15229ad2240d37c71471d35ba7e196cd6163ac28
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-04 07:36:42 +0530

    tuple exercises

--- dp_w3resource_solutions/tuple/21_replace_last_element_of_tuple_of_a_list.py
+++ dp_w3resource_solutions/tuple/21_replace_last_element_of_tuple_of_a_list.py
@@ -8,3 +8,11 @@
 # Sample list: [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
 # Expected Output: [(10, 20, 100), (40, 50, 100), (70, 80, 100)]
 
+alist = [(10, 20, 40), (40, 50, 60), (70, 80, 90)]
+new_list = list()
+
+for each in alist:
+    tup = *each[:-1], 100
+    new_list.append(tup)
+
+print(new_list)
