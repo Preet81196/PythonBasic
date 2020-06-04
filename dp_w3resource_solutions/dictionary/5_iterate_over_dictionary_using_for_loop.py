a583d7182639fcaae30ba1c19af2d59b51ae6980
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-05 00:31:23 +0530

    dictionary exercise

---
+++ dp_w3resource_solutions/dictionary/5_iterate_over_dictionary_using_for_loop.py
@@ -0,0 +1,11 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 05/06/20
+* @decription: Write a Python program to iterate over dictionaries using for loops.
+"""
+
+adict = {1: 'divyesh', 2: 'pareet', 3: 'raj', 4: 'patel'}
+
+for key, value in adict.items():
+    print(key, value)
