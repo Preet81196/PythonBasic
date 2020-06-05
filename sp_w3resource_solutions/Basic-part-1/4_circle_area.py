87058a8a17c91642e92ba1038ddc1be3e2f006c9
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-06-05 08:23:46 +0530

    modi

--- sp_w3resource_solutions/Basic-part-1/4_circle_area.py
+++ sp_w3resource_solutions/Basic-part-1/4_circle_area.py
@@ -1,3 +1,9 @@
+  """Write a Python program which accepts the radius of a circle from the user and compute the area. """
+
+
+
+
+
 from math import pi
 
 radius = float(input("Enter radius : "))
---
+++ sp_w3resource_solutions/Basic-part-1/5_input_firstname_lastname_and_reverse_print.py
@@ -0,0 +1,8 @@
+"""Write a Python program which accepts the user's first and last name and print them in reverse order with a space between them"""
+
+
+
+firstname,lastname  = input("Enter first name and last name:").split()
+
+
+print(lastname + " " + firstname)
