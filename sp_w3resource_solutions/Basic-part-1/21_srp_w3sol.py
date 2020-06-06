74b3b8b0de1bdac940e9857c6a2839108052ad72
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-06-06 10:56:20 +0530

    even odd

---
+++ sp_w3resource_solutions/Basic-part-1/21_srp_w3sol.py
@@ -0,0 +1,8 @@
+"""Write a Python program to find whether a given number (accept from the user) is even or odd, print out an appropriate message to the user  """
+
+num = int(input("enter number:"))
+
+if num % 2:
+    print("number is odd")
+else:
+    print("number is even")
