b4fd180c4c7f55554bb56d3c8b5979cd9b82a41a
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-06-07 08:44:08 +0530

    vowel check

---
+++ sp_w3resource_solutions/Basic-part-1/24_srp_w3sol.py
@@ -0,0 +1,8 @@
+""" Write a Python program to test whether a passed letter is a vowel or not.  """
+
+text = input("Enter string:")
+
+if text == 'a'or text == 'e' or text == 'i' or text == 'o' or text == 'u':
+    print("vowel")
+else:
+    print("not vowel")
