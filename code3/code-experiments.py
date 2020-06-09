c480cfd801722592ed8de0170a13647679bd5fcc
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-09 11:07:33 +0530

    removed .idea and venv folders

---
+++ code3/code-experiments.py
@@ -0,0 +1,6 @@
+"""
+Title: hello earch
+Description: None
+"""
+
+print('Hello Earth')
---
+++ dp_w3resource_solutions/list/33_generate_all_sublists_of_list.py
@@ -0,0 +1,7 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 28/05/20
+* @decription: Write a Python program to generate all sublists of a list.
+"""
+
