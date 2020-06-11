7678a5eab2186d2667a78cb65cb2bf09a6622661
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-11 11:38:28 +0530

    deleted files

--- preet extra/coursera1.py
+++
@@ -1,9 +0,0 @@
-def computepay(hrs,rate):
-    if hrs > 40 :
-        rate_1 = (rate * 1.5) * (hrs - 40)
-    return ((hrs - 5) * rate) +  rate_1
-
-hrs = float(input("Enter Hours:"))
-rate = float(input("Enter Rate:"))
-p = computepay(45,10.50)
-print("Pay",p)
