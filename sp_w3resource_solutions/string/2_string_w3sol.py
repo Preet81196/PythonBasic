fe21862b06c746965cb5755753a5d98bf1972fc4
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-06-17 16:40:42 +0530

    frequency count string

--- sp_w3resource_solutions/string/2_string_w3sol.py
+++ sp_w3resource_solutions/string/2_string_w3sol.py
@@ -1,1 +1,17 @@
 """   Write a Python program to count the number of characters (character frequency) in a string."""
+
+# Python3 code to demonstrate
+# each occurrence frequency using
+# collections.Counter()
+from collections import Counter
+
+# initializing string
+test_str = input("enter string: ")
+
+# using collections.Counter() to get
+# count of each element in string
+res = Counter(test_str)
+
+# printing result
+print("Count of all characters :\n "
+      + str(res))
