245a72f77312f2b9a07a547cd43d12bc41d05f8e
DivYesh Patel <pateldivyesh009@gmail.com>
2020-06-29 11:25:01 +0530

    sqlite3 exercises

---
+++ dp_w3resource_solutions/sqlite-database/9_update_table_column_vals.py
@@ -0,0 +1,32 @@
+"""
+* @author: Divyesh Patel
+* @email: pateldivyesh009@gmail.com 
+* @date: 29/06/20
+* @decription: Write a Python program to update a specific column value of a given table and select
+            all rows before and after updating the said table.
+"""
+
+import sqlite3
+
+
+def display_table_rows():
+    cur.execute('select * from ingredients')
+
+    all_rows = cur.fetchall()
+    for each in all_rows:
+        print(each)
+
+
+connection = sqlite3.connect('testing.sqlite')
+cur = connection.cursor()
+
+display_table_rows()
+
+print('updating...')
+
+update_data_query = 'update ingredients set recipe_id = 5'
+cur.execute(update_data_query)
+
+display_table_rows()
+connection.commit()
+cur.close()
