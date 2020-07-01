df8f8952d24bfa590f375bd4cdc7aee5790b6f5a
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-07-01 15:47:37 +0530

    sets

---
+++ sp_w3resource_solutions/sets/1_create_set.py
@@ -0,0 +1,7 @@
+""" Write a Python program to create a set"""
+
+x = set()
+print(x)
+
+n = set([0, 1, 2, 3, 4])
+print(n)
---
+++ sp_w3resource_solutions/sets/2_iterate_set.py
@@ -0,0 +1,5 @@
+"""Write a Python program to iteration over sets."""
+n = set([0, 1, 2, 3, 4])
+
+for each in n:
+    print(each)
