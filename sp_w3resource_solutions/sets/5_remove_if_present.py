21a2d73c467e25313b1b1b6626570087b7d6fac4
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-07-01 15:56:47 +0530

    remove if present

---
+++ sp_w3resource_solutions/sets/5_remove_if_present.py
@@ -0,0 +1,7 @@
+"""Write a Python program to remove an item from a set if it is present in the set. """
+
+vowels = {'a', 'e', 'i', 'o'}
+
+print(vowels)
+vowels.discard('o')
+print('Vowels are:', vowels)
