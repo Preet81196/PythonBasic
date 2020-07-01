e50120f7d31b6210b6ae490d8c089b03c238592d
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-07-01 16:24:33 +0530

    min max

---
+++ sp_w3resource_solutions/sets/14_max_min.py
@@ -0,0 +1,8 @@
+"""Write a Python program to find maximum and the minimum value in a set."""
+
+number = [3, 2, 8, 5, 10, 6]
+largest_number = max(number);
+smallest_number = min(number);
+
+print("The largest number is:", largest_number)
+print("The smallesh number is :",smallest_number)
