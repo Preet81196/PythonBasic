c7b39299d20d4528289db621a6e4f86e9fdde673
Preet <preet81196@gmail.com>
2020-07-19 16:08:07 +0530

    update

--- pp_w3resource_solutions/flask/webpage.py
+++ pp_w3resource_solutions/flask/webpage.py
@@ -1,9 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
-@app.route("/")
 
+@app.route("/")
 def welcome():
     return "my first webpage"
+
+@app.route("/user")
+def user():
+    return "Enter user name"
 
 if __name__ == "__main__":
     app.run()
