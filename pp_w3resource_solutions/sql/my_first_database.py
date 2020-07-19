e9f76f5465627d0c1988aba011592850d53f9e26
Preet <preet81196@gmail.com>
2020-07-19 14:08:20 +0530

    update

--- pp_w3resource_solutions/sql/my_first_database.py
+++ pp_w3resource_solutions/sql/my_first_database.py
@@ -1,14 +1,14 @@
 import _sqlite3
-conn = _sqlite3.connect("table.db")
 
+
+conn = _sqlite3.connect("table.db")
 c = conn.cursor()
 
 
 c.execute("select * from user")
-
-print(c.fetchall())
-
+data = c.fetchall()
+for items in data:
+    print(items)
 
 conn.commit()
-
 conn.close()
