2db38326a9dac35228122c60f723b7d07c063c27
Preet <preet81196@gmail.com>
2020-07-25 10:18:01 +0530

    create_db

---
+++ pp_w3resource_solutions/sql/check_version_create_db.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+def create_connection(db_file):
+    Conn = sqlite3.connect(db_file)
+    cur =  Conn.cursor()
+
+    print(sqlite3.version)
+    cur.close()
+
+create_connection("version.db")
+
