4f4438ad650656f68968ba34906df25540271232
srp2210 <31180795+srp2210@users.noreply.github.com>
2020-07-28 11:12:35 +0530

    Create 5_extract_urls.py

---
+++ sp_w3resource_solutions/web_scraping/beautiful_soup/5_extract_urls.py
@@ -0,0 +1,13 @@
+"""Write a Python program to extract all the URLs from the webpage python.org that are nested within <li> tags from"""
+
+import requests
+from bs4 import BeautifulSoup
+url = 'https://www.python.org/'
+reqs = requests.get(url)
+soup = BeautifulSoup(reqs.text, 'lxml')
+
+urls = []
+for h in soup.find_all('li'):
+    a = h.find('a')
+    urls.append(a.attrs['href'])
+print(urls)
